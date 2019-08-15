446dc9ca787360f6ee2d6a2f0b9721f5f0d257b4
tsuyosasaki <tsuyo.sasaki@gmail.com>
2019-08-15 19:12:56 +0900

    :sparkles: feat: qiitaのデータを表示するよう対応

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,59 +1,75 @@
 <template lang="pug">
-  .container
-    div
-      Logo
-      h1.title nuxt-typescript-app
-      h2.subtitle My stellar Nuxt.js project
-      .links
-        a(
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        ) Documentation
-        a(
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        ) GitHub
+  .container-block
+    template(v-if="items && items.length > 0")
+      ul.list-wrap
+        li.list-item(v-for="item in items" :key="item.id")
+          h4.title
+            span {{item.title}}
+            small by {{item.user.id}}
+          .item
+            .user
+              img.image(:src="item.user.profile_image_url" alt="")
+            .item-block
+              div.description {{item.body.slice(0, 130)}} ...
+              p.link
+                a(:href="item.url") {{item.url}}
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
-import Logo from '@/components/Logo.vue'
 
-@Component({
-  components: {
-    Logo
+@Component
+export default class IndexPage extends Vue {
+  // public items = ''
+
+  /** ライフサイクル */
+  public async asyncData({ query, $axios }): Promise<any> {
+    const items = await $axios.$get(
+      'http://qiita.com/api/v2/items?query=tag:nuxt.js'
+    )
+
+    console.log('items', items)
+
+    return { items }
   }
-})
-// @Component
-export default class IndexPage extends Vue {}
+
+  public description(item: Record<string | number, any>): string {
+    console.log('description item', item)
+    if (!item) {
+      return ''
+    }
+
+    return item.body.slice(0, 130)
+  }
+}
 </script>
 
 <style lang="sass">
-.container
-  margin: 0 auto
+.container-block
   min-height: 100vh
+  padding: 16px
+
+.list-item + .list-item
+  margin: 16px 0
+
+.list-item
+  > .title
+    margin: 16px 0
+    padding: 8px 0
+    border-bottom: solid 1px #e5e5e5
+
+.item
   display: flex
-  justify-content: center
-  align-items: center
-  text-align: center
 
-.title
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif
-  display: block
-  font-weight: 300
-  font-size: 100px
-  color: #35495e
-  letter-spacing: 1px
+  > .user
+    flex: 120px 0 0
+    width: 120px
+    margin: 5px 20px 5px 0
+  > .user > .image
+    width: 100%
+    object-fit: cover
 
-.subtitle
-  font-weight: 300
-  font-size: 42px
-  color: #526488
-  word-spacing: 5px
-  padding-bottom: 15px
-
-.links
-  padding-top: 15px
+.item-block
+  > .link
+    margin: 8px 0
 </style>
