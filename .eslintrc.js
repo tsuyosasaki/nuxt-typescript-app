d3dcafbc3fec52c490f867e2ced96c4cc7a379b6
tsuyosasaki <tsuyo.sasaki@gmail.com>
2019-08-15 17:18:42 +0900

    :wrench: chore: eslintのルールを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   parser:  'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
     sourceType: 'module',
     ecmaFeatures: {
       legacyDecorators: true
@@ -26,5 +26,29 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'dot-notation': 'off',
+    'no-console': 'off',
+    'no-control-regex': 'off',
+    'no-undef': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'import/no-mutable-exports': 'off',
+    'space-before-function-paren': 'off',
+    // https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/html-self-closing.md
+    'vue/html-self-closing': ['error', {
+      html: {
+        void: 'always',
+        normal: 'never',
+        component: 'always'
+      },
+      svg: 'always',
+      math: 'always'
+    }]
   },
 }
